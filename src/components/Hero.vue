0bcd362f5f8280d335cef8f7aeb30ce1c0149bee
Obez99 <giuliogx@live.it>
2021-11-10 15:36:54 +0100

    (bonus) Adds jumbotron

---
+++ src/components/Hero.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hero">
+    <img
+      src="https://d585tldpucybw.cloudfront.net/sfimages/default-source/default-album/vuel_870x220b06daf62b8fc47d0bbe9d392170bca13.png?sfvrsn=ebc0cf0d_1"
+      alt="vue-jumbo"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<style lang="scss">
+.hero {
+  width: 1000px;
+  height: 400px;
+  margin: auto;
+}
+
+.hero img {
+  width: 100%;
+  height: 100%;
+}
+</style>
